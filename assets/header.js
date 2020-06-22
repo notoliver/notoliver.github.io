1a2942cefbd475e185e8793f789c56ebb6048d49
notoliver <47568829+notoliver@users.noreply.github.com>
2020-06-22 13:34:58 -0400

    responsive af

--- assets/header.js
+++ assets/header.js
@@ -1,4 +1,5 @@
 var level = document.getElementById("header").getAttribute("level");
+var active = document.getElementById("header").getAttribute("active");
 var path =""
 
 if(level==0){
@@ -11,18 +12,39 @@ if(level==2){
     path="../../"
 }
 
+//class=\"active\"
 //console.log(path);
 
-var text ="<div class=\"page\" style=\"wid"+
-"th:30%;justify-content: flex-end;\">\r\n\t\t\t<div clas"+
-"s=\"page link\"><a href=\""+path+"sheetslessons\" class=\"icon\">sheets\/les"+
-"sons<\/a><\/div>\r\n\t\t\t<div class=\"spacer\"><\/div>\r\n\t\t\t<div cla"+
-"ss=\"page link\"><a href=\""+path+"galleries\" class=\"icon\">galleri"+
-"es<\/a><\/div>\r\n\t\t\t<div class=\"spacer\"><\/div>\r\n\t\t<\/d"+
-"iv>\r\n\t\t<div class=\"page\" ><a href=\""+path+"\"><img src=\'"+path+"assets\/oliverqiu2.png\' a"+
-"lt=\'lp4\' height=\'120\' \/><\/a><\/div>\r\n\t\t<div class=\"page\" style=\"width:30%;\">\r\n\t\t\t<d"+
-"iv class=\"spacer\"><\/div>\r\n\t\t\t<div class=\"page link\"><a href=\""+path+"music\" class=\"icon\">mu"+
-"sic<\/a><\/div>\r\n\t\t\t<div class=\"spacer\"><\/div>\r\n\t\t\t<div class=\"page link\"><a href=\""+path+"art\" cl"+
-"ass=\"icon\">art<\/a><\/div>\r\n\t\t\t<div class=\"spacer\"><\/div>\r\n\t\t\t<div class=\"page link\"><a href=\""+path+"about\" class=\"icon\">about<\/a><\/div>\r\n\t\t<\/div>";
+var text ="<a href=\""+path+"\"><img src=\""+path+"assets\/oliverqiu2.png\" height=100px\/><\/a>\r\n\t\t\r\n\t\t<div class=\"topnav\" id=\"myTopnav\">\r\n\t\t\t<a href=\""+path+"music\">Music<\/a>\r\n\t\t\t<a href=\""+path+"sheetslessons\">Sheets\/Lessons<\/a>\r\n\t\t\t<a href=\""+path+"galleries\">Galleries<\/a>\r\n\t\t\t<a href=\""+path+"art\">Art<\/a>\r\n\t\t\t<a href=\""+path+"about\">About<\/a>\r\n\t\t\t<img src=\""+path+"assets\/menuicon.png\" class=\"menu\" onclick=\"openNavModal()\" style=\"height:30px;\"\/>\r\n\t\t<\/div>\r\n\r\n\t\t<div id=\"myNavModal\" class=\"navmodal\">\r\n\t\t\t<span class=\"close cursor\" onclick=\"closeNavModal()\">&times;<\/span>\r\n\t\t\t<div class=\"navmodal-content\">\r\n\t\t\t\t<a href=\""+path+"music\">Music<\/a>\r\n\t\t\t\t<br>\r\n\t\t\t\t<a href=\""+path+"sheetslessons\">Sheets\/Lessons<\/a>\r\n\t\t\t\t<br>\r\n\t\t\t\t<a href=\""+path+"galleries\">Galleries<\/a>\r\n\t\t\t\t<br>\r\n\t\t\t\t<a href=\""+path+"art\">Art<\/a>\r\n\t\t\t\t<br>\r\n\t\t\t\t<a href=\""+path+"about\">About<\/a>\r\n\t\t\t\t<br>\r\n\t\t\t<\/div>\r\n\t\t<\/div>";
 
-document.getElementById("header").innerHTML = text;
+function getIndicesOf(searchStr, str, caseSensitive) {
+    var searchStrLen = searchStr.length;
+    if (searchStrLen == 0) {
+        return [];
+    }
+    var startIndex = 0, index, indices = [];
+    if (!caseSensitive) {
+        str = str.toLowerCase();
+        searchStr = searchStr.toLowerCase();
+    }
+    while ((index = str.indexOf(searchStr, startIndex)) > -1) {
+        indices.push(index);
+        startIndex = index + searchStrLen;
+    }
+    return indices;
+}
+
+var results = getIndicesOf(active, text,true);
+var firstoccurance=results[0]+active.length+1;
+var secondoccurance=results[1]+active.length+1;
+
+var text2 = text.slice(0, firstoccurance) + " class=\"active\" " + text.slice(firstoccurance,secondoccurance)+ " class=\"active\" " +text.slice(secondoccurance);
+
+document.getElementById("header").innerHTML = text2;
+
+function openNavModal() {
+    document.getElementById("myNavModal").style.display = "block";
+}
+function closeNavModal() {
+    document.getElementById("myNavModal").style.display = "none";
+}
