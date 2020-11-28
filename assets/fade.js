9ba803c0b96a952bd1e7c8eb5a4174348339b29e
notoliver <47568829+notoliver@users.noreply.github.com>
2020-11-28 12:48:27 -0500

    FIX THE PDF FUCKING FADE

--- assets/fade.js
+++ assets/fade.js
@@ -11,7 +11,8 @@ document.addEventListener('DOMContentLoaded', function() {
     for (var idx=0; idx<anchors.length; idx+=1) {
         if (anchors[idx].hostname !== window.location.hostname ||
             anchors[idx].pathname === window.location.pathname ||
-            anchors[idx].pathname.indexOf(".pdf">-1)) {
+            anchors[idx].pathname.indexOf(".pdf")>-1) {
+            console.log("what");
             continue;
         }
         anchors[idx].addEventListener('click', function(event) {
