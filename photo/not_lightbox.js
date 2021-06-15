756508ade31341e29bcc59ce98b8cd04abd1a142
notoliver <47568829+notoliver@users.noreply.github.com>
2021-06-15 13:51:05 -0400

    new photogrid

--- photo/not_lightbox.js
+++ photo/not_lightbox.js
@@ -1,112 +1,134 @@
-var num = document.getElementById('modal-content').getAttribute('num');
 var captions=[{},
     {
+        'image': '1.jpg',
         'title':'Waves',
         'source':'Life is Strange (2015)',
         'url':'https://live.staticflickr.com/65535/50208725541_eeec2537a2_o_d.png',
         'dimensions':'6000 x 4000'
     },
     {
+        'image': '2.jpg',
         'title':'Two Whales',
         'source':'Life is Strange (2015)',
         'url':'https://live.staticflickr.com/65535/50208723096_a1cb251c8a_o_d.png',
         'dimensions':'4000 x 6000'
     },
     {
+        'image': '3.jpg',
         'title':'Amber',
         'source':'Life is Strange: Before the Storm (2017)',
         'url':'https://live.staticflickr.com/65535/50581033398_d8d62c80f0_o_d.png',
         'dimensions':'4000 x 6000'
     },
     {
+        'image': '4.jpg',
         'title':'Light',
         'source':'Life is Strange: Before the Storm (2017)',
         'url':'https://live.staticflickr.com/65535/50581759231_dc5f7dc943_o_d.png',
         'dimensions':'4000 x 6000'
     },
     {
+        'image': '5.jpg',
         'title':'Movement',
         'source':'Life is Strange (2015)',
         'url':'https://live.staticflickr.com/65535/50208194153_f631335650_o_d.png',
         'dimensions':'7680 x 4320'
     },
     {
+        'image': '6.jpg',
         'title':'R&R',
         'source':'Life is Strange 2 (2018)',
         'url':'https://live.staticflickr.com/65535/49979414851_7fff23fbfc_o_d.png',
         'dimensions':'6720 x 3780'
     },
     {
+        'image': '7.jpg',
         'title':'Overlook',
         'source':'Tell Me Why (2020)',
         'url':'https://live.staticflickr.com/65535/50651196122_e299dd7cc5_o_d.png',
         'dimensions':'7680 x 4320'
     },
     {
+        'image': '8.jpg',
         'title':'Lakeside',
         'source':'Tell Me Why (2020)',
         'url':'https://live.staticflickr.com/65535/50651202327_d483081a60_o_d.png',
         'dimensions':'7680 x 4320'
     },
     {
+        'image': '9.jpg',
         'title':'Personnel',
         'source':'Tell Me Why (2020)',
         'url':'https://live.staticflickr.com/65535/50648494548_7484ca3fbe_o_d.png',
         'dimensions':'7680 x 4320'
     },
     {
+        'image': '10.jpg',
         'title':'Parked',
         'source':'Tell Me Why (2020)',
         'url':'https://live.staticflickr.com/65535/50650362393_22cd196722_o_d.png',
         'dimensions':'7680 x 4320'
     },
     {
+        'image': '11.jpg',
         'title':'Rocks',
         'source':'Life is Strange 2 (2018)',
         'url':'https://live.staticflickr.com/65535/49979414001_484d02f4b6_o_d.png',
         'dimensions':'7680 x 4320'
     },
     {
+        'image': '12.jpg',
         'title':'Puerto Lobos',
         'source':'Life is Strange 2 (2018)',
         'url':'https://live.staticflickr.com/65535/49978892758_4c2a001d01_o_d.png',
         'dimensions':'7680 x 4320'
     },
     {
+        'image': '13.jpg',
         'title':'Path',
         'source':'Life is Strange 2 (2018)',
         'url':'https://live.staticflickr.com/65535/49978895108_ca493d38dd_o_d.png',
         'dimensions':'11520 x 4216'
     }
-    
 ];
 
-var text ='';
-for(var i=1;i<=num;i++){
-    text+='<div class=\'mySlides\'>\r\n\t<img src=\''+i+'.jpg\' \/>\r\n\t<di'+
-    'v class=\'overlay\'>\r\n\t\t<div class=\'nav left\' onclick=\'plusSlid'+
-    'es(-1)\'><a class=\'prev\' >&#10094;<\/a><\/di'+
-    'v>\r\n\t\t<div class=\'nav right\' onclick=\'plusSlides(1)\'><a clas'+
-    's=\'next\' >&#10095;<\/a><\/div>\r\n\t<\/div>\r\n'+
-    '<div class=\'caption\'>\r\n'+captions[i].title+' &nbsp| &nbsp'+captions[i].source+' &nbsp| &nbsp<a href=\''+captions[i].url+'\'>'+captions[i].dimensions+'<\/a>'+'<\/div>'
-    +'<\/div>';
-}
-document.getElementById('modal-content').innerHTML = text;
+/* #region lightbox output */
+var text ='<span class=\'close cursor\' onclick=\'closeModal()\'>&times;<\/span><div id=\'modal-content\' class=\'modal-content\'>';
 
+for(var i=1;i<captions.length;i++){
+    text+='<div class=\'mySlides\'>'
+    text+='<img src=\''+captions[i].image+'\' \/>'
+    text+='<div class=\'overlay\'>'
+        text+='<div class=\'nav left\' onclick=\'plusSlides(-1)\'>'
+            text+='<a class=\'prev\' >&#10094;<\/a><\/div>'
+        text+='<div class=\'nav right\' onclick=\'plusSlides(1)\'>'
+            text+='<a class=\'next\' >&#10095;<\/a><\/div>'
+    text+='<\/div><div class=\'caption\'>'+captions[i].title+' &nbsp| &nbsp'+captions[i].source+' &nbsp| &nbsp<a href=\''+captions[i].url+'\'>'+captions[i].dimensions+'<\/a>'+'<\/div>'
+    text+='<\/div>';   
+}
+
+text+='<\/div>'
+document.getElementById('myModal').innerHTML = text;
+/* #endregion */
+
+/* #region functions */
 function openModal() {
     document.getElementById('myModal').style.display = 'block';
 }
 function closeModal() {
     document.getElementById('myModal').style.display = 'none';
 }
+
 var slideIndex = 1;
-showSlides(slideIndex);
+
 function plusSlides(n) {
     showSlides(slideIndex += n);
-}	
+}
+
 function currentSlide(n) {
     showSlides(slideIndex = n);
 }
+
 function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName('mySlides');
@@ -119,6 +141,33 @@ function showSlides(n) {
     slides[slideIndex-1].style.display = 'block';
 }
 
+function resizeLightbox(){
+    var stuff = document.querySelectorAll('.mySlides > img');
+    var w = window.innerWidth;
+    var h = window.innerHeight;
+    if(w>h){//constant height
+        for(var i=0;i<stuff.length;i++){
+            if(i==6||i==12){
+                //console.log('wide'); TEMPORARY SHITTY FIXLOL 
+                stuff[i].style.height='auto';
+                stuff[i].style.width=w-50+'px';
+                continue;
+            }
+            stuff[i].style.width='auto';
+            stuff[i].style.height=h-100+'px';
+        }
+    }
+    if(h>w){//constant width
+        for(var i=0;i<stuff.length;i++){
+            stuff[i].style.height='auto';
+            stuff[i].style.width=w-20+'px';
+        }
+    }
+}
+
+/* #endregion */
+
+/* #region open close */
 $('body').on('click', function(event) {
     closeModal();
 });
@@ -139,3 +188,6 @@ document.addEventListener('keydown', function(event) {
         plusSlides(1);
     }
 });
+/* #endregion */
+
+resizeLightbox();
